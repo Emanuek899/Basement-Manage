5af0572e09f9bd8410824a387f78d07e8e80f3bd
Emanuel <emanuel.carbajalsantacruz@gmail.com>
2025-07-01 23:37:22 -0600

    allowed hosts

--- settings/settings.py
+++ settings/settings.py
@@ -135,3 +135,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+ALLOWED_HOSTS = ['web-production-b0fee.up.railway.app']
