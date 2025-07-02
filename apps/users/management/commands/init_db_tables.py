fe010577681b8e50fbb5770326ea4dc82a48276d
Emanuel <emanuel.carbajalsantacruz@gmail.com>
2025-07-02 00:15:28 -0600

    ultimo commit

--- apps/users/management/commands/init_db_tables.py
+++ apps/users/management/commands/init_db_tables.py
@@ -5,8 +5,10 @@ class Command(BaseCommand):
     help = "Crea todas las tablas necesarias en la base de datos."
 
     def handle(self, *args, **kwargs):
+        self.stdout.write('Iniciando creación de tablas...')
         try:
-            run_all_creations()
+            run_all_creations(self.stdout)
             self.stdout.write(self.style.SUCCESS("Todas las tablas fueron creadas correctamente."))
         except Exception as e:
             self.stderr.write(self.style.ERROR(f"Error: {e}"))
+            raise  # Muy importante para que el error no se oculte y Railway lo detecte
